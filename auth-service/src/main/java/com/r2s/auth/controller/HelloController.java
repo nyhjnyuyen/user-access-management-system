9dab47bb90ecf424f6c7be148fcf73719c218f30
nyhjnyuyen <ntn14204@uga.edu>
2026-01-19 08:36:36 -0500

    feat: implement auth register login and jwt security

--- auth-service/src/main/java/com/r2s/auth/controller/HelloController.java
+++ auth-service/src/main/java/com/r2s/auth/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.r2s.auth;
+package com.r2s.auth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Auth Service";
+        return "Hello protected API";
     }
 }

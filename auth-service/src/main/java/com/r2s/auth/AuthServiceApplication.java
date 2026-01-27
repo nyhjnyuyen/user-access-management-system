5256dd53ebfa12a5c8b542c3133c40c31cb42e0f
nyhjnyuyen <ntn14204@uga.edu>
2026-01-26 22:12:21 -0500

    feat(part-5) : initial implementation

--- auth-service/src/main/java/com/r2s/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/r2s/auth/AuthServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = {"com.r2s.auth", "com.r2s.core"})
 @EntityScan(basePackages = "com.r2s.core")
-public class AuthServiceApplication {
+public class  AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
